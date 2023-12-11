83f53579607c288d5c84dd3857de16913c257dd8
Hannah2609 <hann7419@stud.kea.dk>
2023-12-11 19:36:46 +0100

    mæh

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,13 +1,77 @@
-import React from 'react'
+import { useState } from "react";
+import Cards from "react-credit-cards-2";
 
 
-function Payment() {
+const Payment = () => {
+  const [state, setState] = useState({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "",
+    focus: "",
+  });
+
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleInputFocus = (evt) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
+  };
+
   return (
-    <fieldset>
-      <h1>PAYMENT</h1>
+    <div>
+      <Cards
+        number={state.number}
+        expiry={state.expiry}
+        cvc={state.cvc}
+        name={state.name}
+        focused={state.focus}
+      />
 
-    </fieldset>
+      <form>
+        <input
+          type="number"
+          name="number"
+          placeholder="Kortnummer"
+          required
+          value={state.number}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Kortholder navn"
+          required
+          value={state.name}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="date"
+          name="udløb"
+          placeholder="Gyldig til:"
+          required
+          value={state.expiry}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="number"
+          name="cvc"
+          placeholder="CVC"
+          required
+          pattern="\d{3,4}"
+          value={state.cvc}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+      </form>
+    </div>
   );
-}
+};
 
-export default Payment
+export default Payment;
