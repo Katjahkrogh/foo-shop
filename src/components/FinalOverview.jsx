b0cf3002872e6dfc74ec5741098fd303bcd9a845
Hannah2609 <hann7419@stud.kea.dk>
2023-12-12 16:25:24 +0100

    ændringer

--- src/components/FinalOverview.jsx
+++ src/components/FinalOverview.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Bebas_Neue } from "next/font/google";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 function FinalOverview() {
   return (
-    <div>FinalOverview</div>
-  )
+    <div>
+      <h2 className={`${bebasNeue.className} text-3xl text-fooYellow-200 mb-4`}>
+        {" "}
+        OVERVIEW
+      </h2>
+    </div>
+  );
 }
 
-export default FinalOverview
+export default FinalOverview;
--- src/components/TicketType.jsx
+++ src/components/TicketType.jsx
@@ -1,3 +1,10 @@
+import { Bebas_Neue } from "next/font/google";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 function TicketType({
   setStep, // funktion til at opdatere trin i bookingprocessen
@@ -7,184 +14,180 @@ function TicketType({
   ticketAmount, // tilstand for antal "foo-billetter"
   setTickets, // funktion til at opdatere billetlisten
   priceVip,
-  priceRegular 
+  priceRegular,
 }) {
   const totalTicketAmount = ticketAmount; // Total antal "foo-billetter"
   const totalVipAmount = vipAmount; // Total antal VIP-billetter
 
   return (
-    <fieldset className="m-10 p-10">
-        <h2 className="text-fooYellow-200 text-3xl">FOOFEST </h2>
-        <p>Mandag d. 10 Juli - Søndag d. 17 Juli 2024</p>
-        <legend className="text-fooYellow-200 text-xl mt-4 mb-2">
-          VÆLG DINE BILLETTER
-        </legend>
+    <fieldset>
+      <h2
+        className={`${bebasNeue.className} text-3xl text-fooYellow-200 mb-4`}
+      >
+        VÆLG DINE BILLETTER
+      </h2>
 
-        {/* FOO-BILLET sektion */}
-        <div className="flex justify-between">
-          <div>
-            <h3>FOO-BILLET</h3>
-            <p>799 DKK</p>
-          </div>
-
-          <label className="flex items-center" htmlFor="foo-billet">
-            {/* FJERN 1 FOO-BILLET  */}
-            <button
-              type="button"
-              aria-label={`Fjern 1 foo-billet`}
-              onClick={() => {
-                if (ticketAmount > 0) {
-                  setTicketAmount(ticketAmount - 1);
-                  setTickets((obj) => {
-                    const removeFromBasket = obj.findIndex(
-                      (ticket) => ticket.price === priceRegular
-                    );
-                    if (removeFromBasket !== -1) {
-                      return obj.filter(
-                        (_, index) => index !== removeFromBasket
-                      );
-                    } else {
-                      return obj;
-                    }
-                  });
-                }
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                fill="currentColor"
-                class="bi bi-dash-circle"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
-              </svg>
-            </button>
-
-            {/* INPUT FOO-BILLET */}
-            <input
-              className="text-black mx-4 appearance-none border p-2 rounded w-20"
-              type="number"
-              min={0}
-              name="billet"
-              id="foo-billet"
-              value={totalTicketAmount}
-            />
-
-            {/* TILFØJ 1 FOO-BILLet */}
-            <button
-              type="button"
-              aria-label={`Tilføj 1 foo-billet`}
-              onClick={() => {
-                setTicketAmount(ticketAmount + 1);
-                setTickets((obj) =>
-                  obj.concat({
-                    ticketName: "FOO-billet",
-                    id: obj.length,
-                    price: priceRegular,
-                  })
-                );
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                fill="currentColor"
-                class="bi bi-plus-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
-              </svg>
-            </button>
-          </label>
+      {/* FOO-BILLET sektion */}
+      <div className="flex justify-between">
+        <div>
+          <h3>FOO-BILLET</h3>
+          <p>799 DKK</p>
         </div>
 
-        {/* VIP-BILLET sektion */}
-        <div className="flex justify-between mt-10">
-          <div>
-            <h3>VIP-BILLET</h3>
-            <p>1299 DKK</p>
-          </div>
-          <label className="flex items-center" htmlFor="vip-billet">
-            {/* FJERN 1 VIP-billet  */}
-            <button
-              type="button"
-              aria-label={`Fjern 1 VIP-billet`}
-              onClick={() => {
-                if (vipAmount > 0) {
-                  setVipAmount(vipAmount - 1);
-                  setTickets((obj) => {
-                    const removeFromBasket = obj.findIndex(
-                      (ticket) => ticket.price === priceVip
-                    );
-                    if (removeFromBasket !== -1) {
-                      return obj.filter(
-                        (_, index) => index !== removeFromBasket
-                      );
-                    } else {
-                      return obj;
-                    }
-                  });
-                }
-              }}
+        <label className="flex items-center" htmlFor="foo-billet">
+          {/* FJERN 1 FOO-BILLET  */}
+          <button
+            type="button"
+            aria-label={`Fjern 1 foo-billet`}
+            onClick={() => {
+              if (ticketAmount > 0) {
+                setTicketAmount(ticketAmount - 1);
+                setTickets((obj) => {
+                  const removeFromBasket = obj.findIndex(
+                    (ticket) => ticket.price === priceRegular
+                  );
+                  if (removeFromBasket !== -1) {
+                    return obj.filter((_, index) => index !== removeFromBasket);
+                  } else {
+                    return obj;
+                  }
+                });
+              }
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="currentColor"
+              class="bi bi-dash-circle"
+              viewBox="0 0 16 16"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                fill="currentColor"
-                class="bi bi-dash-circle"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
-              </svg>
-            </button>
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+            </svg>
+          </button>
 
-            {/* INPUT VIP */}
-            <input
-              className="text-black mx-4 appearance-none border p-2 rounded w-20"
-              type="number"
-              name="billet"
-              id="vip-billet"
-              min={0}
-              value={totalVipAmount}
-            />
+          {/* INPUT FOO-BILLET */}
+          <input
+            className="text-black mx-4 appearance-none border p-2 rounded w-20"
+            type="number"
+            min={0}
+            name="billet"
+            id="foo-billet"
+            value={totalTicketAmount}
+          />
 
-            {/* TILFØJ 1 VIP-billet  */}
-            <button
-              type="button"
-              aria-label={`Tilføj 1 VIP-billet`}
-              onClick={() => {
-                setVipAmount(vipAmount + 1);
-                setTickets((obj) =>
-                  obj.concat({
-                    ticketName: "VIP-billet",
-                    id: obj.length,
-                    price: priceVip,
-                  })
-                );
-              }}
+          {/* TILFØJ 1 FOO-BILLet */}
+          <button
+            type="button"
+            aria-label={`Tilføj 1 foo-billet`}
+            onClick={() => {
+              setTicketAmount(ticketAmount + 1);
+              setTickets((obj) =>
+                obj.concat({
+                  ticketName: "FOO-billet",
+                  id: obj.length,
+                  price: priceRegular,
+                })
+              );
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="currentColor"
+              class="bi bi-plus-circle-fill"
+              viewBox="0 0 16 16"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                fill="currentColor"
-                class="bi bi-plus-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
-              </svg>
-            </button>
-          </label>
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+            </svg>
+          </button>
+        </label>
+      </div>
+
+      {/* VIP-BILLET sektion */}
+      <div className="flex justify-between mt-10">
+        <div>
+          <h3>VIP-BILLET</h3>
+          <p>1299 DKK</p>
         </div>
+        <label className="flex items-center" htmlFor="vip-billet">
+          {/* FJERN 1 VIP-billet  */}
+          <button
+            type="button"
+            aria-label={`Fjern 1 VIP-billet`}
+            onClick={() => {
+              if (vipAmount > 0) {
+                setVipAmount(vipAmount - 1);
+                setTickets((obj) => {
+                  const removeFromBasket = obj.findIndex(
+                    (ticket) => ticket.price === priceVip
+                  );
+                  if (removeFromBasket !== -1) {
+                    return obj.filter((_, index) => index !== removeFromBasket);
+                  } else {
+                    return obj;
+                  }
+                });
+              }
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="currentColor"
+              class="bi bi-dash-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+            </svg>
+          </button>
 
-        {/* gå til nyt step */}
-        {/* <div className="flex justify-end">
+          {/* INPUT VIP */}
+          <input
+            className="text-black mx-4 appearance-none border p-2 rounded w-20"
+            type="number"
+            name="billet"
+            id="vip-billet"
+            min={0}
+            value={totalVipAmount}
+          />
+
+          {/* TILFØJ 1 VIP-billet  */}
+          <button
+            type="button"
+            aria-label={`Tilføj 1 VIP-billet`}
+            onClick={() => {
+              setVipAmount(vipAmount + 1);
+              setTickets((obj) =>
+                obj.concat({
+                  ticketName: "VIP-billet",
+                  id: obj.length,
+                  price: priceVip,
+                })
+              );
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="currentColor"
+              class="bi bi-plus-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+            </svg>
+          </button>
+        </label>
+      </div>
+
+      {/* gå til nyt step */}
+      {/* <div className="flex justify-end">
           <NextStepBtn
             setStep={setStep}
             text="FIND MINE BILLETTER"
